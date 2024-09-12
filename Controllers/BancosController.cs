4083715a6de0eb50bbf84eecb3882be85a270e4f
Willian Barata <willianbarata@gmail.com>
2024-09-12 11:54:16 -0300

    Edição do banco concluída

--- Controllers/BancosController.cs
+++ Controllers/BancosController.cs
@@ -171,8 +171,7 @@ namespace projFront.Controllers
         // POST: Bancos/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+        [HttpPut]
         public async Task<IActionResult> Edit(int id, BancoViewModel bancoVM)
         {
             if (id != bancoVM.IdBanco)
